c0969379382a415dd8420570e070f41c1e730127
mahfuzahmmedabir <mahfuz.abir06@gmail.com>
2025-06-02 14:40:11 +0100

    string sort

--- 2 week/string constructor/string_contator.cpp
+++ 2 week/string constructor/string_contator.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    // string s;
+    // cin >> s;
+    // string t(s,3);
+    // string s = "mahfuz";
+    // string t(s,1);
+    // cout<< t;
+
+    // string s(10,'a');
+    // cout << s;
+
+
+    int x = 200;
+    char y = '10';
+    cout << x + y;
     return 0; 
 }
